7aeb956edc260f9e9e70df080942a9a6dfaaa32e
严浩 <1424412158@qq.com>
2020-04-10 15:07:50 +0800

    2

--- huofu1/huofu1.cpp
+++ huofu1/huofu1.cpp
@@ -1,20 +1,36 @@
-﻿// huofu1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	Mat src = imread("c://project//smt.png ");
+	Mat dst;
+	Canny(src, dst, 50, 200, 3);
+	std::vector<cv::Vec2f> lines;
+	cv::HoughLines(dst, lines, 1, CV_PI / 180,100 );
+	cv::Mat lineMat;
+	cv::HoughLines(dst, lines, 1, CV_PI / 180, 100);
+	std::vector<cv::Vec2f>::iterator it = lines.begin();
+	for (; it != lines.end(); ++it)
+	{
+		float rho = (*it)[0], theta = (*it)[1];
+		cv::Point pt1, pt2;
+		double a = cos(theta), b = sin(theta);
+		double x0 = a * rho, y0 = b * rho;
+		pt1.x = cv::saturate_cast<int>(x0 + 1000 * (-b));
+		pt1.y = cv::saturate_cast<int>(y0 + 1000 * (a));
+		pt2.x = cv::saturate_cast<int>(x0 - 1000 * (-b));
+		pt2.y = cv::saturate_cast<int>(y0 - 1000 * (a));
+		cv::line(src, pt1, pt2, cv::Scalar(0, 0, 255), 1, CV_AA);
+	}
+	imshow("src", src);
+	imshow("dst", dst);
+	waitKey(0);
+	return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
